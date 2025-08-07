d69ddc4d88481b3070a0c64af99398f81e00a0da
RishitPradhan <rishit.pradhan6620@gmail.com>
2025-08-08 02:13:01 +0530

    Restructure repository: move all files to root level and ensure new auth code is visible - Remove nested project directory structure - Add rebuilt LoginForm and RegisterForm components at root level - Ensure all deployment fixes and new code are properly visible on GitHub - Fix repository structure for proper deployment and visibility

--- project/src/components/pages/PaymentsPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PremiumPaymentPage from '../payments/PremiumPaymentPage';
-
-const PaymentsPage: React.FC = () => {
-  return <PremiumPaymentPage />;
-};
-
-export default PaymentsPage; 
--- project/src/components/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as ProfilePage } from './ProfilePage';
-export { default as ServicesPage } from './ServicesPage';
-export { default as PortfolioPage } from './PortfolioPage';
-export { default as SkillsPage } from './SkillsPage';
-export { default as MyGigsPage } from './MyGigsPage';
-export { default as MyOrdersPage } from './MyOrdersPage';
-export { default as MessagesPage } from './MessagesPage';
-export { default as NotificationsPage } from './NotificationsPage'; 
